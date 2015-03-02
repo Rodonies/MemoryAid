bee6a9097b8b3aded34a5d7479b98da9bb8371d6
Stijn <stijn.schrauwen@stundent.ap.be>
2015-03-02 13:00:20 +0100

    ?

--- app/src/main/java/com/memoryaid/memoryaid/Settings.java
+++ app/src/main/java/com/memoryaid/memoryaid/Settings.java
@@ -1,7 +1,7 @@
 package com.memoryaid.memoryaid;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
