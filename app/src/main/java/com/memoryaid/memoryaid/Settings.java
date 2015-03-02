d4e8f61166d92bd8e3a3b3b0254263cbc0e5feac
Stijn <stijn.schrauwen@stundent.ap.be>
2015-03-02 12:58:55 +0100

    Java bestanden toevoegen aan hoofdproject

---
+++ app/src/main/java/com/memoryaid/memoryaid/Settings.java
@@ -0,0 +1,146 @@
+package com.memoryaid.memoryaid;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.RadioButton;
+
+
+public class Settings extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        themeUtils.onActivityCreateSetTheme(this);
+        setContentView(R.layout.activity_main);
+
+        final RadioButton A = (RadioButton) findViewById(R.id.RadioSmall);
+        final RadioButton B = (RadioButton) findViewById(R.id.RadioMedium);
+        final RadioButton C = (RadioButton) findViewById(R.id.RadioBig);
+
+        switch(themeUtils.cTheme) {
+            case 1:
+                A.setChecked(true);
+                B.setChecked(false);
+                C.setChecked(false);
+                break;
+            case 0:
+                A.setChecked(false);
+                B.setChecked(false);
+                C.setChecked(true);
+                break;
+            case 2:
+                A.setChecked(false);
+                B.setChecked(true);
+                C.setChecked(false);
+                break;
+        }
+
+           final RadioButton m_one = (RadioButton) findViewById(R.id.RadioEnglish);
+           final RadioButton m_two = (RadioButton) findViewById(R.id.RadioFrans);
+           final RadioButton m_three = (RadioButton) findViewById(R.id.RadioDeutsch);
+           final RadioButton m_four = (RadioButton) findViewById(R.id.RadioEspanol);
+
+
+            m_one.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    m_one.setChecked(true);
+                    m_two.setChecked(false);
+                    m_three.setChecked(false);
+                    m_four.setChecked(false);
+                }
+            });
+
+            m_two.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    m_one.setChecked(false);
+                    m_two.setChecked(true);
+                    m_three.setChecked(false);
+                    m_four.setChecked(false);
+                }
+            });
+
+            m_three.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    m_one.setChecked(false);
+                    m_two.setChecked(false);
+                    m_three.setChecked(true);
+                    m_four.setChecked(false);
+                }
+            });
+
+            m_four.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                m_one.setChecked(false);
+                m_two.setChecked(false);
+                m_three.setChecked(false);
+                m_four.setChecked(true);
+            }
+            });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+
+
+
+    public void onRadioButtonLanguageClicked(View view)
+    {
+        boolean checked = ((RadioButton)view).isChecked();
+
+    }
+
+    public void AddProfClicked(View view) {
+                Log.e("test","iets");
+    }
+
+
+    public void onRadioButtonClicked(View view)
+    {
+        boolean checked = ((RadioButton)view).isChecked();
+
+
+           switch(view.getId()) {
+
+            case R.id.RadioMedium:
+                themeUtils.changeToTheme(this,themeUtils.Medium);
+                break;
+            case R.id.RadioBig:
+                if (checked)
+                themeUtils.changeToTheme(this,themeUtils.Big);
+                    break;
+            case R.id.RadioSmall:
+                if (checked)
+                themeUtils.changeToTheme(this,themeUtils.Small);
+                    break;
+        }
+
+        }
+}
---
+++ app/src/main/java/com/memoryaid/memoryaid/themeUtils.java
@@ -0,0 +1,39 @@
+package com.memoryaid.memoryaid;
+
+/**
+ * Created by stijn on 6/02/2015.
+ */
+
+import android.app.Activity;
+import android.content.Intent;
+
+public class themeUtils {
+    public static int cTheme = 2;
+    public final static int Big = 0;
+    public final static int Small = 1;
+    public final static int Medium = 2;
+
+
+    public static void changeToTheme( Activity activity, int theme)
+    {
+        cTheme = theme;
+        activity.finish();
+        activity.startActivity(new Intent(activity, activity.getClass()));
+    }
+
+    public static void onActivityCreateSetTheme(Activity activity)
+    {
+        switch (cTheme)
+        {
+            case Big:
+                activity.setTheme(R.style.Big);
+                break;
+            case Small:
+                activity.setTheme(R.style.Small);
+                break;
+            case Medium:
+                activity.setTheme(R.style.Medium);
+                break;
+        }
+    }
+}
