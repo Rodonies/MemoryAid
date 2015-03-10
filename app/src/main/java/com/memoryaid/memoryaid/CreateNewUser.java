52a280a17f2e44d1d8050b02117634f77f9d28d0
Vamirax <kerstensbart.bk@gmail.com>
2015-03-10 12:38:49 +0100

    Able to choose where to get photo from: Gallery, Camera or cancel + API 15 -> 18, added some permissions to use camera

--- app/src/main/java/com/memoryaid/memoryaid/CreateNewUser.java
+++ app/src/main/java/com/memoryaid/memoryaid/CreateNewUser.java
@@ -1,6 +1,9 @@
 package com.memoryaid.memoryaid;
 
+import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.media.Image;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -10,31 +13,31 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 
-public class CreateNewUser extends ActionBarActivity {
+public class CreateNewUser extends ActionBarActivity implements View.OnClickListener {
 
 
-
+    private Dialog dialog;
     private ImageView contactImgView;
+    private ImageView imgGallery;
+    private ImageView imgCamera;
     private Button btnAddPhoto;
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-       setContentView(R.layout.activity_create_new_user);
-
-
+         setContentView(R.layout.activity_create_new_user);
         btnAddPhoto = (Button) findViewById(R.id.btnAddPhoto);
         contactImgView = (ImageView) findViewById(R.id.ChosenPhoto);
 
-
-        contactImgView = (ImageView) findViewById(R.id.ChosenPhoto);
-
-
-        setContentView(R.layout.activity_create_new_user);
 
     }
 
@@ -76,17 +79,65 @@ public class CreateNewUser extends ActionBarActivity {
     public void SearchPhoto(View view) {
 
 
-        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-        Uri uri = Uri.parse(Environment.getExternalStorageDirectory().getPath() + "/Camera/");
-
-        intent.setDataAndType(uri, "image/*");
-        startActivityForResult(Intent.createChooser(intent, "Select contact image"),1);
 
 
+        // custom dialog
 
+        dialog = new Dialog(CreateNewUser.this);
+        dialog.setContentView(R.layout.customdialogvenster);
+        dialog.setTitle("Select image source");
+
+        imgGallery = (ImageView) dialog.findViewById(R.id.PhotoGallery);
+        imgCamera = (ImageView) dialog.findViewById(R.id.PhotoCamera);
+
+        Picasso.with(CreateNewUser.this).load(R.drawable.galleryblue).resize(150, 150).into(imgGallery);
+        Picasso.with(CreateNewUser.this).load(R.drawable.camerablue).resize(150,150).into(imgCamera);
+
+        Button dialogButton = (Button) dialog.findViewById(R.id.dialogButtonOK);
+
+        dialogButton.setOnClickListener(this);
+        imgGallery.setOnClickListener(this);
+        imgCamera.setOnClickListener(this);
+
+        dialog.show();
 
 
     }
+
+
+    public void CloseDialog()
+    {
+        dialog.dismiss();
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        CloseDialog();
+
+        switch(v.getId()){
+
+            case R.id.dialogButtonOK: /** Start a new Activity MyCards.java */
+
+
+                break;
+
+            case R.id.PhotoCamera: /** AlerDialog when click on Exit */
+
+                break;
+
+            case R.id.PhotoGallery: /** AlerDialog when click on Exit */
+
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                Uri uri = Uri.parse(Environment.getExternalStorageDirectory().getPath() + "/Camera/");
+                intent.setDataAndType(uri, "image/*");
+                startActivityForResult(Intent.createChooser(intent, "Select contact image"),1);
+
+                break;
+        }
+
+    }
+
 
 
 }
