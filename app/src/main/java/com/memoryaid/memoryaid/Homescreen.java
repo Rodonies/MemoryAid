bd1e6f1b96b4c3187d7bdcacb1567aaac006c537
Vamirax <kerstensbart.bk@gmail.com>
2015-03-16 13:25:05 +0100

    commit: linking all the stuff and things

--- app/src/main/java/com/memoryaid/memoryaid/Homescreen.java
+++ app/src/main/java/com/memoryaid/memoryaid/Homescreen.java
@@ -7,23 +7,41 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 
 import java.util.ArrayList;
 
-public class Homescreen extends ActionBarActivity {
+public class Homescreen extends ActionBarActivity implements View.OnClickListener {
+
+
+    private Button btnViewProfiles;
+    private Button btnProfileManager;
+    private Button btnSettings;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homescreen);
 
-        DatabaseHandler db = new DatabaseHandler(this);
+        btnProfileManager = (Button) findViewById(R.id.btnProfileManager);
+        btnViewProfiles = (Button) findViewById(R.id.btnProfileView);
+        btnSettings = (Button) findViewById(R.id.btnSettings);
 
+        btnSettings.setOnClickListener(this);
+        btnViewProfiles.setOnClickListener(this);
+        btnProfileManager.setOnClickListener(this);
+
+        DatabaseHandler db = new DatabaseHandler(this);
+/*
         db.addProfile(new Profile("Jan", "Jansens", "127.0.0.1"));
         db.addContact(new Contact("Jos", "Jansens", "Family", "13.33.33.37", "Jos Jansens is my nephew"));
 
         if (db.findProfile(1)) {
             db.getProfile().showContacts();
         }
+*/
+
+
     }
 
     public void CreateNewUser(View view) {
@@ -35,7 +53,21 @@ public class Homescreen extends ActionBarActivity {
         Intent i = new Intent(this,Settings.class);
         startActivity(i);
     }
+    public void onClick(View v) {
 
+        switch (v.getId()) {
+            case R.id.btnProfileView: /** AlerDialog when click on Exit */
+                break;
+            case R.id.btnProfileManager: /** AlerDialog when click on Exit */
+                CreateNewUser(v);
+                break;
+            case R.id.btnSettings:
+                Settings(v);
+                break;
+
+        }
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
