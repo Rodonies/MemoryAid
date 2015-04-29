38f141456f1034bd2d5b6789adc2a7b7cd11d510
Vamirax <kerstensbart.bk@gmail.com>
2015-04-29 14:05:39 +0200

    Working on selector for contacts

--- app/src/main/java/com/memoryaid/memoryaid/ProfileView.java
+++ app/src/main/java/com/memoryaid/memoryaid/ProfileView.java
@@ -1,14 +1,11 @@
 package com.memoryaid.memoryaid;
 
 import android.content.SharedPreferences;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -19,6 +16,8 @@ public class ProfileView extends ActionBarActivity {
     public static final String SaveData = "MyPreferenceFiles";
     private int CurrentProfile;
     private String ContactMode;
+
+    private String Name,LastName,Phone,Date_of_Birth,Information,Relation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,29 +63,15 @@ public class ProfileView extends ActionBarActivity {
 
 
     }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_profile_view, menu);
-        return true;
-
+    void ViewProfile(View v)
+    {
+        setContentView(R.layout.activity_profile_view);
+         Name = ((EditText) findViewById(R.id.FirstNameText)).getText().toString();
 
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
 
-        return super.onOptionsItemSelected(item);
-    }
+
+
 }
